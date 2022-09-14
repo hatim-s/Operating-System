712f4b404a2996195fd7e3c042b42fa06432d8e5
Hatim <hatimshakir9179@gmail.com>
2022-09-14 14:54:50 +0530

    Stage 16 Completed.

---
+++ spl/compile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+./spl spl_progs/stage16/bootmodule.spl
+./spl spl_progs/stage16/console.spl
+./spl spl_progs/stage16/devicemanager.spl
+./spl spl_progs/stage16/readsystemcall.spl
+./spl spl_progs/stage16/writesystemcall.spl
+./spl spl_progs/stage16/resourcemanager.spl
+./spl spl_progs/stage16/startupcode.spl
+./spl spl_progs/stage16/exception.spl
