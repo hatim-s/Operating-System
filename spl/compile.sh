d1ec6caa82ff35158009d625203cb1cef1aded79
Hatim <hatimshakir9179@gmail.com>
2022-10-11 00:46:54 +0530

    stage 17 completed

---
+++ spl/compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./spl spl_progs/stage17/bootmodule.spl
+./spl spl_progs/stage17/startupcode.spl
+./spl spl_progs/stage17/exec.spl
+./spl spl_progs/stage17/processmanager.spl
+./spl spl_progs/stage17/memorymanager.spl
+./spl spl_progs/stage16/exception.spl
