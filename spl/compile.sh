37e1b4d1cc7bc147f2c2900347a6f78889578c23
Hatim <hatimshakir9179@gmail.com>
2022-10-13 15:48:39 +0530

    stage 16 reorganised

--- spl/compile.sh
+++ spl/compile.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-./spl spl_progs/stage18/exception.spl
-./spl spl_progs/stage18/int-console.spl
-./spl spl_progs/stage18/int-disk.spl
-./spl spl_progs/stage18/int-timer.spl
-./spl spl_progs/stage18/int6-read.spl
-./spl spl_progs/stage18/int7-write.spl
-./spl spl_progs/stage18/int9-exec.spl
-./spl spl_progs/stage18/int10-halt.spl
-./spl spl_progs/stage18/mod0-resource.spl
-./spl spl_progs/stage18/mod1-process.spl
-./spl spl_progs/stage18/mod2-memory.spl
-./spl spl_progs/stage18/mod4-device.spl
-./spl spl_progs/stage18/mod5-scheduler.spl
-./spl spl_progs/stage18/mod7-boot.spl
-./spl spl_progs/stage18/startup.spl
+./spl spl_progs/stage16/exception.spl
+./spl spl_progs/stage16/int-console.spl
+./spl spl_progs/stage16/int-timer.spl
+./spl spl_progs/stage16/int6-read.spl
+./spl spl_progs/stage16/int7-write.spl
+./spl spl_progs/stage16/int10-halt.spl
+./spl spl_progs/stage16/mod0-resource.spl
+./spl spl_progs/stage16/mod4-device.spl
+./spl spl_progs/stage16/mod5-scheduler.spl
+./spl spl_progs/stage16/mod7-boot.spl
+./spl spl_progs/stage16/startup.spl
